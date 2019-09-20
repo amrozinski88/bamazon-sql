74c0a03ee87104abb5ea79f6377709734aad1fbc
AdamM <ajmrozinski@gmail.com>
2019-09-20 01:12:37 -0400

    added prompt for which item id and purchase amount

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,12 +13,30 @@ const connection = mysql.createConnection({
 
 
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log(`connected as id ${connection.threadId} `)
-})
 
-const getAll = function () {
+
+
+const promptBuyer = () => {
+
+    inquirer.prompt([{
+        type: "input",
+        name: "id",
+        message: "Please enter the ID # of the item you want to purchase:",
+        validate: input => isNaN(input) === true ? false : true
+    },
+    {
+        type: "input",
+        name: "amount",
+        message: "Please enter how many you would like to purchase: ",
+        validate: input => isNaN(input) === true ? false : true
+    }]).then(answers=>{ 
+
+    })
+
+};
+
+
+const getAll = () => {
     connection.query("SELECT * FROM products", (err, res) => {
         if (err) throw err;
         console.log(res[0].product_name)
@@ -26,7 +44,21 @@ const getAll = function () {
             console.log(`--------------------------------------------`)
             console.log(`ID #:  ${item_id} ,Product Name: ${product_name}, Price: ${price}`)
         })
+        promptBuyer()
     })
+
 }
 
-getAll()
+
+
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId} `)
+    getAll()
+});
+
+
+
+// * The first should ask them the ID of the product they would like to buy.
+//    * The second message should ask how many units of the product they would like to buy.
